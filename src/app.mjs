bb8810d05554a21338d96b98786be532c8c6a2fa
ritawi8 <rita.wildrot@gmail.com>
2025-05-29 14:47:22 +0200

    felhantering och logs klar.

--- src/app.mjs
+++ src/app.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { logger } from './utilities/logger.mjs';
 import errorHandler from './middleware/errorHandler.mjs';
+import routes from './routes/blockchain-routes.mjs';
 
 dotenv.config({ path: './config/config.env' });
 
@@ -11,10 +12,13 @@ app.use(express.json());
 
 app.use(logger);
 
+app.use('/api/blocks', routes);
+
 // Hantera 404-fel (endpoint finns inte)
 app.use((req, res, next) => {
 	const error = new Error(`Not Found - ${req.originalUrl}`);
 	error.statusCode = 404;
+	error.status = 'Not Found';
 	next(error); // Skicka vidare till error handlern
 });
 
